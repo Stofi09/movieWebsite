5c9fc3f8e44a7574360a26cc36a9d9747af762e5
Kris <kristof.solak@gmail.com>
2021-11-25 19:32:25 +0100

    asd

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "firstreact": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
